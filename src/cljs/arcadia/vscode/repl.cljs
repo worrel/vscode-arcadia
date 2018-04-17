d520c650149036302c8a1ad5c7abf36c96d4c5c8
Ulysses Popple <ulysses.popple@gmail.com>
2018-04-17 10:28:10 -0500

    Prevent harmless errors
    https://github.com/arcadia-unity/Arcadia/issues/225
    https://github.com/arcadia-unity/Repl.exe/commit/347052d7373ec1e4268447743b65db0f94cd36cd

--- src/cljs/arcadia/vscode/repl.cljs
+++ src/cljs/arcadia/vscode/repl.cljs
@@ -48,7 +48,7 @@
             (println "; Arcadia REPL")
             (println (str "; Clojure " (clojure-version)))
             (println (str "; Unity " (UnityEditorInternal.InternalEditorUtility/GetFullUnityVersion)))
-            (println (str "; Mono " (.Invoke (.GetMethod Mono.Runtime "GetDisplayName" (enum-or BindingFlags/NonPublic BindingFlags/Static)) nil nil)))))))
+            (println (str "; Mono " (.Invoke (.GetMethod Mono.Runtime "GetDisplayName" (enum-or System.Reflection.BindingFlags/NonPublic System.Reflection.BindingFlags/Static)) nil nil)))))))
 
 (defn connect-repl 
  [output host port]
